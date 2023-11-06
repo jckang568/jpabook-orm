6fe1d2bdf62a97571345d4b8dd2428fa90d7f756
jckang <jckang568@gmail.com>
2023-11-06 23:28:34 +0900

    Initial commit

---
+++ src/main/java/kr/co/jayce/jpabookorm/JpabookOrmApplication.java
@@ -0,0 +1,13 @@
+package kr.co.jayce.jpabookorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpabookOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpabookOrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/jayce/jpabookorm/JpabookOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.jayce.jpabookorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpabookOrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
